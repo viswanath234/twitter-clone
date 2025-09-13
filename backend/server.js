8b1391de159a5411193f3a6c2b2ceb30425eec7d
viswanath234 <viswabai859@gmail.com>
2025-09-13 16:07:38 +0530

    vercel setup 6

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "",
+    origin: "https://twitter-clone-dtj8.vercel.app/login",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
