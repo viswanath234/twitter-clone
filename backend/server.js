47840ae23b0ede56c785c61090e01ab4dcb220f1
viswanath234 <viswabai859@gmail.com>
2025-09-13 16:04:24 +0530

    vercel setup 5

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "https://xclone-zeta.vercel.app",
+    origin: "",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
