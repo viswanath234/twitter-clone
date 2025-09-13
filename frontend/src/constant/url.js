5136a44edabbb06a63f3043ac80d02edbb49d782
viswanath234 <viswabai859@gmail.com>
2025-09-13 18:28:44 +0530

    lie url

--- frontend/src/constant/url.js
+++ frontend/src/constant/url.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:5000";
-// export const baseUrl = "https://x-clone-h9fl.onrender.com";
+// export const baseUrl = "http://localhost:5000";
+export const baseUrl = "https://x-clone-dq39.onrender.com";
